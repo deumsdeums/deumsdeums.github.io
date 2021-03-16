d0c19458c66852c0367777e4f898676e7696df90
deumsdeums <deumsgame@gmail.com>
2021-03-16 17:13:22 +0100

    fetch

--- scripts/script.js
+++ scripts/script.js
@@ -4,13 +4,7 @@ window.onload=function(){
 
 function loadAlmanax(){
     let request = new XMLHttpRequest();
-    request.onreadystatechange=function(){
-        console.log("Etat "+this.readyState +" Code : "+this.status)
-        if(this.readyState==4&&this.status==200)
-            console.log(this.response);
-        
-    }
-    request.open("GET","localhost:8080/almanax",true)
-    request.send();
+    fetch("localhost:8080/almanax").then(response => console.log(response))
+
 
 }
