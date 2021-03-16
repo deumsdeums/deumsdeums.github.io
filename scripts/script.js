39d9929d879be2026833e4bfe95049edb62ef26a
deumsdeums <deumsgame@gmail.com>
2021-03-16 20:02:44 +0100

    almanax

--- scripts/script.js
+++ scripts/script.js
@@ -3,5 +3,17 @@ window.onload=function(){
 }
 
 function loadAlmanax(){
-    fetch("https://deumsserver.herokuapp.com/almanax").then(response => console.log(response))
+    fetch("https://deumsserver.herokuapp.com/almanax").then(response => response.json())
+    .then(almanax =>{
+        document.getElementById("offrandeTitle").append(document.createElement("h2").textContent=almanax.bonusTitle)
+        document.getElementById("offrandeBonus").append(document.createElement("p").textContent=almanax.bonus)
+        document.getElementById("offrandeObjectif").append(document.createElement("p").textContent=almanax.objectif)
+        let itemImg = new Image(50,50)
+        itemImg.src=almanax.img
+        document.getElementById("offrandeImg").append(itemImg)
+
+
+
+    })
+    
 }
