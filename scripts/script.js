651a1d58b24af1568e47ec050dd27a760fb7f034
deumsdeums <deumsgame@gmail.com>
2021-03-16 17:00:15 +0100

    fix

--- scripts/script.js
+++ scripts/script.js
@@ -6,9 +6,9 @@ function loadAlmanax(){
     let request = new XMLHttpRequest();
     request.onreadystatechange=function(){
         console.log("Etat "+this.readyState +" Code : "+this.status)
-        if(this.readyState==4&&this.status==200){
+        if(this.readyState==4&&this.status==200)
             console.log(this.response);
-        }
+        
     }
     request.open("GET","localhost:8080/almanax",true)
     request.send();
